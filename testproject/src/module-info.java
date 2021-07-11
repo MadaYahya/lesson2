adfe9b3688c8298f99fbff04eb37e2707f1d7b4e
MadaYahya <87212570+MadaYahya@users.noreply.github.com>
2021-07-11 05:14:22 +0300

    module homework2

--- testproject/src/module-info.java
+++ testproject/src/module-info.java
@@ -1,2 +1,34 @@
 module testproject {
+	
+
+
+import java.util.regex.Pattern;
+		  
+		public class homework2 
+		{
+		
+		    public static boolean isValid(String email)
+		    {
+		        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+		                            "[a-zA-Z0-9_+&*-]+)*@" +
+		                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+		                            "A-Z]{2,7}$";
+		                              
+		        Pattern pat = Pattern.compile(emailRegex);
+		        if (email == null)
+		            return false;
+		        return pat.matcher(email).matches();
+		    }
+		  
+		    /* driver function to check */
+		    public static void main(String[] args)
+		    {
+		        String email = "adam@simplilearn.org";
+		        if (isValid(email))
+		            System.out.print("Yes");
+		        else
+		            System.out.print("No");
+		    }
+		}
+
 }
